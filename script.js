b930a010b1c09b134f0da382bddec358a31c1955
Rishab-Rawat <rawatrishab66@gmail.com>
2024-07-18 20:52:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,28 @@
 function convertToRoman(num) {
-    if (num < 1 || num > 100000) {
-        return '';
+  const romanSymbols = [
+    ['M', 1000],
+    ['CM', 900],
+    ['D', 500],
+    ['CD', 400],
+    ['C', 100],
+    ['XC', 90],
+    ['L', 50],
+    ['XL', 40],
+    ['X', 10],
+    ['IX', 9],
+    ['V', 5],
+    ['IV', 4],
+    ['I', 1]
+  ];
+
+  let romanNumeral = '';
+
+  for (const [symbol, value] of romanSymbols) {
+    while (num >= value) {
+      romanNumeral += symbol;
+      num -= value;
     }
-    
-    const romanSymbols = [
-        ['M', 1000],
-        ['CM', 900],
-        ['D', 500],
-        ['CD', 400],
-        ['C', 100],
-        ['XC', 90],
-        ['L', 50],
-        ['XL', 40],
-        ['X', 10],
-        ['IX', 9],
-        ['V', 5],
-        ['IV', 4],
-        ['I', 1]
-    ];
-    
-    let roman = '';
-    
-    for (let [symbol, value] of romanSymbols) {
-        while (num >= value) {
-            roman += symbol;
-            num -= value;
-        }
-    }
-    
-    return roman;
+  }
+
+  return romanNumeral;
 }
